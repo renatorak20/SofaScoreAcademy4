d91b239b9f04bebf6f9dea1b857045486623e1d0
RennSter20 <88338561+RennSter20@users.noreply.github.com>
2023-03-16 20:17:56 +0100

    Added random color generator for every company object in recycler.

--- app/src/main/java/com/renato/sofascoreacademy4/adapters/CompanyRecyclerAdapter.kt
+++ app/src/main/java/com/renato/sofascoreacademy4/adapters/CompanyRecyclerAdapter.kt
@@ -1,6 +1,8 @@
 package com.renato.sofascoreacademy4.adapters
 
 import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,6 +13,7 @@ import com.renato.sofascoreacademy4.R
 import com.renato.sofascoreacademy4.databinding.CompanyObjectBinding
 import com.renato.sofascoreacademy4.entities.Company
 import java.util.zip.Inflater
+import kotlin.random.Random
 
 class CompanyRecyclerAdapter(val context:Context, val listOfItems:ArrayList<Company>) : RecyclerView.Adapter<CompanyRecyclerAdapter.RecyclerViewHolder>() {
 
@@ -31,8 +34,10 @@ class CompanyRecyclerAdapter(val context:Context, val listOfItems:ArrayList<Comp
         holder.binding.imageView.load(company.website){
             transformations(CircleCropTransformation())
             crossfade(true)
-            crossfade(100)
+            crossfade(2000)
         }
+        //smanjio sam alpha da boje ne budu jako izrazene
+        holder.binding.layout.setBackgroundColor(Color.argb(100, Random.nextInt(256), Random.nextInt(256), Random.nextInt(256)))
     }
 
     override fun getItemCount() = listOfItems.size
