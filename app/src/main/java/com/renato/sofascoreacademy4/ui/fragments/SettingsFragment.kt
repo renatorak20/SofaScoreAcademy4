c40d279ec48899af9734214ad05e4a35a865d23c
RennSter20 <88338561+RennSter20@users.noreply.github.com>
2023-03-22 11:48:53 +0100

    Added preference constants, fixed settings layout, moved demo objects to viewModel.

--- app/src/main/java/com/renato/sofascoreacademy4/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/renato/sofascoreacademy4/ui/fragments/SettingsFragment.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.SharedPreferences
 import android.content.res.Configuration
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
